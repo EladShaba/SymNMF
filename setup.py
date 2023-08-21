299b8c252c7309cf9ac488a9ba5387f1cbfda933
IdorosinerTau <117019553+IdorosinerTau@users.noreply.github.com>
2023-08-21 21:38:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import Extension, setup
 
-module = Extension("symnmf", sources=['symnmf.c','symnmfmodule.c'])
-setup(name='symnmf',
+module = Extension("symnmfmod", sources=['symnmf.c','symnmfmodule.c'])
+setup(name='symnmfmod',
      version='1.0',
      description='Python wrapper for custom C extension',
      ext_modules=[module])
