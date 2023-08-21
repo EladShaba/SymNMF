01512d0f36a94f6eb487788f26dc98eaf54a5c8d
IdorosinerTau <117019553+IdorosinerTau@users.noreply.github.com>
2023-08-21 21:39:34 +0300

    Update symnmf.h

--- symnmf.h
+++ symnmf.h
@@ -1,5 +1,6 @@
 # ifndef symnmfh
 # define symnmfh
+# define _CRT_SECURE_NO_WARNINGS 1
 
 double** create_zero_mat(int n, int d);
 void freeMat(double **mat, int n);
@@ -7,12 +8,12 @@ double** arr_cast_mat(double *arr, int n, int d);
 double** create_A_mat(double **points, int n, int d);
 double** create_D_mat(double **A_mat, int n);
 double** D_mat_normalized(double **A_mat, int n);
-double** mat_multipication(double **matA, double **matB, int n);
+double** mat_multipication(double **matA, double **matB, int n, int m, int d);
 double** create_W_mat(double **points, int n, int d);
 void printm(double **mat, int n, int d);
-double** transpose(double **mat, int n);
+double** transpose(double **mat, int n, int d);
 double frobenius_norm(double **mat, int n, int k);
-void get_points_input(FILE *ifp, double* points_arr);
+double** get_points_input(FILE *ifp, int n, int d);
 double** update_H_mat(double **matH, double **matW, int n, int k, double epsilon, int iter);
 
 
