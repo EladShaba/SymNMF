402004576ddc0a9a154d1144e58b7a4365585cb9
EladShaba <116885767+EladShaba@users.noreply.github.com>
2024-06-23 02:34:47 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import Extension, setup
-
-module = Extension("symnmfmod", sources=['symnmf.c','symnmfmodule.c'])
-setup(name='symnmfmod',
-     version='1.0',
-     description='Python wrapper for custom C extension',
-     ext_modules=[module])
